919020c9971b8d3978277f22b6ab9d92620d34ee
leigh-sh <leigh.shaniv@gmail.com>
2014-02-03 00:31:03 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
-twit = Flask(__name__)
-twit.config.from_object('config')
-db = SQLAlchemy(app) 
-
-from twit import views, models
-
