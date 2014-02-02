c1fdae68018175325caded3ae7eeb818594dfe6e
leigh-sh <leigh.shaniv@gmail.com>
2014-02-03 00:53:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-##The database URI that will be used for the connection    
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
