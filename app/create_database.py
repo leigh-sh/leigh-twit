373230f423a7caff6b2c7322d2685c3c5116614e
leigh-sh <leigh.shaniv@gmail.com>
2014-02-03 00:38:32 +0200

    Delete create_database.py

--- app/create_database.py
+++
@@ -1,7 +0,0 @@
-#!flask/bin/python
-from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from app import db
-import os.path
-
-db.create_all() ##opens the connection to the database
